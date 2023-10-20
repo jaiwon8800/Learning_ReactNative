3d871a027ebb4fd6147158c137e566c1157ae20b
JW Learning Repository <148437683+jaiwon8800@users.noreply.github.com>
2023-10-20 23:00:17 +0900

    [React Native] 스타일링 (인라인, StyleSheet 객체 스타일링)

--- 231019_rn_first_pj/App.js
+++ 231019_rn_first_pj/App.js
@@ -4,9 +4,13 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>RN 프로젝트 렛츠고</Text>
+        <Text style={styles.dummyText}>RN 프로젝트 렛츠고</Text>
       </View>
-      <Text>Hello World! 재원</Text>
+      {/* 인라인 스타일링 방식 */}
+      {/* <Text
+        style={{ margin: 16, borderWidth: 2, borderColor: "red", padding: 16 }}
+      > */}
+      <Text style={styles.dummyText}>Hello World! 재원</Text>
       <Button title="클릭하세요" />
     </View>
   );
@@ -18,5 +22,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: "blue",
   },
 });

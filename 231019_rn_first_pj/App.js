5083ad7333e3a3eafc0bbb8453e8f85ef45b8e56
JW Learning Repository <148437683+jaiwon8800@users.noreply.github.com>
2023-10-20 22:40:22 +0900

    [React Native] 핵심 컴포넌트(Text, View...)

--- 231019_rn_first_pj/App.js
+++ 231019_rn_first_pj/App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text>RN 프로젝트 렛츠고</Text>
+      </View>
       <Text>Hello World! 재원</Text>
-      <StatusBar style="auto" />
+      <Button title="클릭하세요" />
     </View>
   );
 }
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

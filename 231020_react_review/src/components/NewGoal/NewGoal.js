f7bdeb24d20333ea3a06f25753f039fbb0ebed94
JW Learning Repository <148437683+jaiwon8800@users.noreply.github.com>
2023-10-20 21:55:51 +0900

    [React] 사용자 입력, 양방향 바인딩

--- 231020_react_review/src/components/NewGoal/NewGoal.js
+++ 231020_react_review/src/components/NewGoal/NewGoal.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./NewGoal.css";
 
 const NewGoal = (props) => {
+  const [enteredText, setEnteredText] = useState("");
   const addGoalHandler = (event) => {
     // 브라우저 기본 값이 백엔드 요청 보내는 것을 막음
     event.preventDefault();
 
     const newGoal = {
       id: Math.random().toString(),
-      text: "My new goal!",
+      text: enteredText,
     };
+
+    setEnteredText("");
+
     props.onAddGoal(newGoal);
+  };
+
+  const textChangeHandler = (event) => {
+    setEnteredText(event.target.value);
   };
 
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" onChange={textChangeHandler} />
       <button type="submit">목표 추가하기</button>
     </form>
   );

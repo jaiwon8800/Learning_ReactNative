f85c5271dbb4f2b00924e825dd30b91bc339e566
JW Learning Repository <148437683+jaiwon8800@users.noreply.github.com>
2023-10-21 23:37:21 +0900

    [React Native] View, 레이아웃, 플렉스박스

--- 231019_rn_first_pj/App.js
+++ 231019_rn_first_pj/App.js
@@ -1,32 +1,42 @@
 import { StyleSheet, Text, View, Button } from "react-native";
+import { TextInput } from "react-native-web";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>RN 프로젝트 렛츠고</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="강의 목표를 입력하세요"
+        />
+        <Button title="목표 추가하기" />
       </View>
-      {/* 인라인 스타일링 방식 */}
-      {/* <Text
-        style={{ margin: 16, borderWidth: 2, borderColor: "red", padding: 16 }}
-      > */}
-      <Text style={styles.dummyText}>Hello World! 재원</Text>
-      <Button title="클릭하세요" />
+      <View>
+        <Text>목표 리스트</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
   },
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: "blue",
+  inputContainer: {
+    flexDirection: "row",
+    // (row일 때)행 안에서 정렬되는 방식
+    // (column일 때)열 안에서 정렬되는 방식
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
   },
 });
+
+// RN에서의 View는 특정 스타일을 설정하지 않아도 기본값으로 플렉스 박스를 사용함
+// RN에서의 플렉스 박스의 기본값은 세로 정렬
+// 웹에서의 플렉스 박스의 기본값은 가로 정렬
